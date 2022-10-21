00b0a889265667c763faf54570a59b007264c36c
heatwaven <markopavlovic205051@gmail.com>
2022-10-21 21:18:15 +0200

    Client and Server

--- server.cpp
+++ server.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
-#include <unistd.h>
-#include <assert.h>
-#include <vector>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <typeinfo>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-
-// socket includes
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
 
 using namespace std;
+
+#define BUF 1024
+#define PORT 6543
+
+int abortRequested = 0;
+int create_socket = -1;
+int new_socket = -1;
+
+void *clientCommunication(void *data);
+void signalHandler(int sig);
 
 int main(int argc, char *argv[])
 {
@@ -28,14 +36,26 @@ int main(int argc, char *argv[])
         if (optind < argc)
             mailDirectoryName = argv[optind++];
         else
+        {
             cout << "<mail-spool-directoryname> wurde nicht eingeben\n";
+            return -1;
+        }
     }
     else
     {
         cout << "Es wurden keine Parameter eingegeben\n";
+        return -1;
     }
 
-    int create_socket;
+    socklen_t addrlen;
+    struct sockaddr_in address, cliaddress;
+    int reuseValue = 1;
+
+    if (signal(SIGINT, signalHandler) == SIG_ERR)
+    {
+        perror("signal can not be registered");
+        return EXIT_FAILURE;
+    }
 
     if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -43,13 +63,30 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    socklen_t addrlen;
-    struct sockaddr_in address, cliaddress;
+    if (setsockopt(create_socket,
+                   SOL_SOCKET,
+                   SO_REUSEADDR,
+                   &reuseValue,
+                   sizeof(reuseValue)) == -1)
+    {
+        perror("set socket options - reuseAddr");
+        return EXIT_FAILURE;
+    }
 
-    // memset(&address, 0, sizeof(address));
+    if (setsockopt(create_socket,
+                   SOL_SOCKET,
+                   SO_REUSEPORT,
+                   &reuseValue,
+                   sizeof(reuseValue)) == -1)
+    {
+        perror("set socket options - reusePort");
+        return EXIT_FAILURE;
+    }
+
+    memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+    address.sin_port = htons(PORT);
 
     if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1)
     {
@@ -62,10 +99,6 @@ int main(int argc, char *argv[])
         perror("listen error");
         return EXIT_FAILURE;
     }
-
-    int abortRequested = 0;
-    // int create_socket = -1; 
-    int new_socket = -1;
 
     while (!abortRequested)
     {
@@ -88,12 +121,138 @@ int main(int argc, char *argv[])
             break;
         }
 
-
         printf("Client connected from %s:%d...\n",
                inet_ntoa(cliaddress.sin_addr),
                ntohs(cliaddress.sin_port));
-        // clientCommunication(&new_socket); // returnValue can be ignored
+        clientCommunication(&new_socket); // returnValue can be ignored
         new_socket = -1;
     }
-    return 0;
+
+    if (create_socket != -1)
+    {
+        if (shutdown(create_socket, SHUT_RDWR) == -1)
+        {
+            perror("shutdown create_socket");
+        }
+        if (close(create_socket) == -1)
+        {
+            perror("close create_socket");
+        }
+        create_socket = -1;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void *clientCommunication(void *data)
+{
+    char buffer[BUF];
+    int size;
+    int *current_socket = (int *)data;
+
+    ////////////////////////////////////////////////////////////////////////////
+    // SEND welcome message
+    strcpy(buffer, "Welcome to myserver!\r\nPlease enter your commands...\r\n");
+    if (send(*current_socket, buffer, strlen(buffer), 0) == -1)
+    {
+        perror("send failed");
+        return NULL;
+    }
+
+    do
+    {
+
+        size = recv(*current_socket, buffer, BUF - 1, 0);
+        if (size == -1)
+        {
+            if (abortRequested)
+            {
+                perror("recv error after aborted");
+            }
+            else
+            {
+                perror("recv error");
+            }
+            break;
+        }
+
+        if (size == 0)
+        {
+            printf("Client closed remote socket\n"); // ignore error
+            break;
+        }
+
+        // remove ugly debug message, because of the sent newline of client
+        if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
+        {
+            size -= 2;
+        }
+        else if (buffer[size - 1] == '\n')
+        {
+            --size;
+        }
+
+        buffer[size] = '\0';
+        printf("Message received: %s\n", buffer); // ignore error
+
+        if (send(*current_socket, "OK", 3, 0) == -1)
+        {
+            perror("send answer failed");
+            return NULL;
+        }
+    } while (strcmp(buffer, "quit") != 0 && !abortRequested);
+
+    if (*current_socket != -1)
+    {
+        if (shutdown(*current_socket, SHUT_RDWR) == -1)
+        {
+            perror("shutdown new_socket");
+        }
+        if (close(*current_socket) == -1)
+        {
+            perror("close new_socket");
+        }
+        *current_socket = -1;
+    }
+
+    return NULL;
+}
+
+void signalHandler(int sig)
+{
+    if (sig == SIGINT)
+    {
+        printf("abort Requested... \n"); // ignore error
+        abortRequested = 1;
+
+        if (new_socket != -1)
+        {
+            if (shutdown(new_socket, SHUT_RDWR) == -1)
+            {
+                perror("shutdown new_socket");
+            }
+            if (close(new_socket) == -1)
+            {
+                perror("close new_socket");
+            }
+            new_socket = -1;
+        }
+
+        if (create_socket != -1)
+        {
+            if (shutdown(create_socket, SHUT_RDWR) == -1)
+            {
+                perror("shutdown create_socket");
+            }
+            if (close(create_socket) == -1)
+            {
+                perror("close create_socket");
+            }
+            create_socket = -1;
+        }
+    }
+    else
+    {
+        exit(sig);
+    }
 }
